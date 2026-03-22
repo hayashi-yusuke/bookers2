592ed632c655ab06afc1a8605aa9d051b0bc6984
hayashi-yusuke <262776262+hayashi-yusuke@users.noreply.github.com>
2026-03-22 05:20:56 +0000

    add favorite model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_17_075949) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_22_044346) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -43,6 +43,13 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_17_075949) do
     t.text "body"
     t.datetime "created_at", null: false
     t.string "title"
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "book_id"
+    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
   end
