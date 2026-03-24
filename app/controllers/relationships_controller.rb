04892034f068f2612a2b0687b1ab7ca87bcdaf60
hayashi-yusuke <262776262+hayashi-yusuke@users.noreply.github.com>
2026-03-24 04:29:49 +0000

    Add relationships controller

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,25 @@
 class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    user = User.find(params[:user_id])
+    Current_user.follow(user)
+    redirect_to request.referer
+  end
+
+  def destroy
+    user = User.find(params[:user_id])
+    Current_user.unfollow(user)
+    redirect_to request.referer
+  end
+
+  def followings
+    user = User.find(params[:user_id])
+    @users = user.followings
+  end
+
+  def followers
+    user = User.find(params[:user_id])
+    @user = user.followers
+  end
 end
