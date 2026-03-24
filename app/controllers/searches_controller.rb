f4090841fd1e4f3978d31fe9df07cf6193afff37
hayashi-yusuke <262776262+hayashi-yusuke@users.noreply.github.com>
2026-03-24 12:26:52 +0000

    Add search feature

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,30 @@
+class SearchesController < ApplicationController
+
+  def search
+    @model = params[:model]
+    @content = params[:content]
+    @method = params[:method]
+
+    if @model == "User"
+      @records = search_model(User, :name)
+    elsif @model == "Book"
+      @records = search_model(Book, :title)
+    end
+  end
+
+  private
+
+  def search_model(model, column)
+    case @method
+    when "perfect"
+      model.where(column => @content)
+    when "forward"
+      model.where("#{column} LIKE ?", "#{@content}%")
+    when "backward"
+      model.where("#{column} LIKE ?", "%#{@content}")
+    when "partial"
+      model.where("#{column} LIKE ?", "%#{@content}%")
+    end
+  end
+
+end
